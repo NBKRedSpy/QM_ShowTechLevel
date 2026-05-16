0ece45c844c96bb319625bfb58d40af925e041a3
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2026-05-15 21:06:19 -0400

    WIP

--- src/TooltipFactory_BuildMissionTooltip_Patch.cs
+++ src/TooltipFactory_BuildMissionTooltip_Patch.cs
@@ -3,6 +3,7 @@ using MGSC;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -27,9 +28,52 @@ namespace QM_ShowTechLevel
             }
 
 
-            Faction faction = Plugin.State.Get<Factions>().Get(mission.BeneficiaryFactionId);
-            __instance.AddPanelToTooltip().LocalizeName("tooltip.Power").SetValue($"{(faction.Power / 1000f):0.#}K");
-            __instance.AddPanelToTooltip().LocalizeName("tooltip.TechLevel").SetValue(faction.CurrentTechLevel);
+            //Hack to reset the text.
+
+            //__instance.AddPanelToTooltip().LocalizeName("tooltip.Power").SetValue($"{(faction.Power / 1000f):0.#}K");
+            //__instance.AddPanelToTooltip().LocalizeName("tooltip.TechLevel").SetValue(faction.CurrentTechLevel);
+
+
+            SetTechLevelText(__instance._usedPropertyPanels, "common_beneficiary", mission.BeneficiaryFactionId);
+
+            SetTechLevelText(__instance._usedPropertyPanels, "common_panic", mission.VictimFactionId);
+
+
+
+            //TooltipProperty property = null;
+
+            //property = __instance._usedPropertyPanels
+            //    .Where(x => x.Icon.sprite == Data.TooltipIcons.GetSpriteByTag("common_beneficiary")).FirstOrDefault();
+
+            //if(property != null)
+            //{
+            //    Faction faction = Plugin.State.Get<Factions>().Get(mission.BeneficiaryFactionId);
+
+            //    string text = property.Value.text;
+            //    string newText = $"<size=70%>{faction.CurrentTechLevel} / {(faction.Power/1000f):0.#}K</size> {text}";
+
+            //    property.SetValue(newText, false);
+            //}
+
+            ////__instance._usedPropertyPanels[0].Icon.sprite  == Data.TooltipIcons.GetSpriteByTag("common_beneficiary")
+        }
+
+        private static void SetTechLevelText(List<TooltipProperty> usedPropertyPanels, string spriteId, string factionId)
+        {
+            TooltipProperty property = null;
+
+            property = usedPropertyPanels
+                .Where(x => x.Icon.sprite == Data.TooltipIcons.GetSpriteByTag(spriteId)).FirstOrDefault();
+
+            if (property == null) return;
+
+            Faction faction = Plugin.State.Get<Factions>().Get(factionId);
+
+            string text = property.Value.text;
+            string newText = $"<size=70%>{faction.CurrentTechLevel} / {(faction.Power / 1000f):0.#}K</size> {text}";
+
+            property.SetValue(newText, false);
+
         }
 
     }
